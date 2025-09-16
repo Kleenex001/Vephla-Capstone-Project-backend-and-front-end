478726f295cf89e7f9fc97b9301f092feaa1833e
Jessey Joel <jjessey21@gmail.com>
2025-09-16 15:30:27 +0100

    changes to morgan

--- Biz-Boost-main/Backend/app.js
+++ Biz-Boost-main/Backend/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
 
 // Load environment variables
 dotenv.config();
