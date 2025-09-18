859e863506d13226fdcb12987737dbf3cce4b2b5
Jessey Joel <jjessey21@gmail.com>
2025-09-18 18:53:07 +0100

    update

--- public/sales.js
+++ public/sales.js
@@ -139,7 +139,7 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
 
       const paymentEl = document.getElementById("paymentType");
-      const statusEl = document.getElementById("Status");
+      const statusEl = document.getElementById("status");
       if (!paymentEl || !statusEl) {
         showToast("❌ Form elements missing!", "error");
         return;
